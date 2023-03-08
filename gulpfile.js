307875fc1cf33e864f8826d7bb08f8990321acc7
rafaelranery <rafaelrapnery@gmail.com>
2023-03-08 01:04:53 -0300

    gulp compileJs function

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require('gulp');
 /* Plugins for sass */
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-
+/* Plugins for Js compiling */
+const uglify = require('gulp-uglify');
 
 function compileSass() {
     return gulp.src('./source/styles/*.scss')
@@ -12,7 +13,15 @@ function compileSass() {
         .pipe(gulp.dest('./build/styles'))
 }
 
+function compileJs() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
 exports.compileSass = compileSass;
+exports.compileJs = compileJs;
+
 
 exports.default = function() {
     gulp.watch('./source/styles/main.scss', {ignoreInitial: false}, compileSass);
